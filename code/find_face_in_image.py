215d52f2df96f1fdbc69cb87144bacc4e1e71409
JeffreyLewis-cd <jeffreylewiscd@gmail.com>
2020-11-04 22:23:09 +0800

    in sync

--- code/find_face_in_image.py
+++ code/find_face_in_image.py
@@ -5,10 +5,10 @@ import numpy as np
 
 img_url_02 = '../images/a/'
 img_url_03 = '../images/b/'
-lena = mpimg.imread(img_url_02) # 读取和代码处于同一目录下的 lena.png
-lena.shape #(512, 512, 3)
-plt.imshow(lena) # 显示图片
-plt.axis('off') # 不显示坐标轴
-plt.show()
+# lena = mpimg.imread(img_url_02) # 读取和代码处于同一目录下的 lena.png
+# lena.shape #(512, 512, 3)
+# plt.imshow(lena) # 显示图片
+# plt.axis('off') # 不显示坐标轴
+# plt.show()
 v_res = gabor_face_recognition.get_hist(img_url_02, img_url_03)
 print(v_res)
